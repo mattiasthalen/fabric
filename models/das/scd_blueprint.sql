04880b8c3a9090e6c920867acc07161ae33ad8b5
Mattias Thalén <bitter-polders0x@icloud.com>
2025-06-26 07:33:48 +0000

    Format scd blueprint

--- models/das/scd_blueprint.sql
+++ models/das/scd_blueprint.sql
@@ -6,29 +6,30 @@ MODEL (
     columns [_record__hash],
     valid_from_name _record__valid_from,
     valid_to_name _record__valid_to,
-    invalidate_hard_deletes true,
-    updated_at_name _record__loaded_at,
+    invalidate_hard_deletes TRUE,
+    updated_at_name _record__loaded_at
   ),
   blueprints (
     (source := northwind__categories, @unique_key := category_id),
     (source := northwind__category_details, @unique_key := category_id),
     (source := northwind__customers, @unique_key := customer_id),
     (source := northwind__employees, @unique_key := employee_id),
-    (source := northwind__employee_territories, @unique_key := (_get_northwindapiv_1_employees_employee_id, territory_id)),
+    (
+      source := northwind__employee_territories,
+      @unique_key := (_get_northwindapiv_1_employees_employee_id, territory_id)
+    ),
     (source := northwind__order_details, @unique_key := (order_id, product_id)),
     (source := northwind__orders, @unique_key := order_id),
     (source := northwind__products, @unique_key := product_id),
     (source := northwind__regions, @unique_key := region_id),
     (source := northwind__shippers, @unique_key := shipper_id),
     (source := northwind__suppliers, @unique_key := supplier_id),
-    (source := northwind__territories, @unique_key := territory_id),
+    (source := northwind__territories, @unique_key := territory_id)
   )
 );
 
 SELECT
-  @STAR__LIST(
-    table_name := landing_zone.northwind.raw__@source
-  ),
+  @STAR__LIST(table_name := landing_zone.northwind.raw__@source),
   @GENERATE_SURROGATE_KEY(
     @STAR__LIST(
       table_name := landing_zone.northwind.raw__@source,
