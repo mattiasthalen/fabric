3ff31654f71f713ad6134e9c7d3c8a7cf81e7392
Mattias Thalén <bitter-polders0x@icloud.com>
2025-06-27 08:34:05 +0000

    Refactor environment variable handling

--- dlt/northwind/northwind.py
+++ dlt/northwind/northwind.py
@@ -173,12 +173,12 @@ def load_northwind(env) -> None:
 
     if dev_mode:
         branch_name = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip().decode('utf-8')
-        dataset_name = f"dev__{dataset_name}__{branch_name.replace('-', '_')}"
+        dataset_name = f"dev_{dataset_name}_{branch_name.replace('-', '_')}"
 
-    workspace_id = os.getenv("FABRIC__WORKSPACE_ID")
-    lakehouse_id = os.getenv("FABRIC__LAKEHOUSE_ID")
+    #workspace_id = os.getenv("FABRIC__WORKSPACE_ID", "Missing")
+    #lakehouse_id = os.getenv("FABRIC__LAKEHOUSE_ID", "Missing")
 
-    bucket_url = f"abfss://{workspace_id}@onelake.dfs.fabric.microsoft.com/{lakehouse_id}/Tables"
+    #bucket_url = f"abfss://{workspace_id}@onelake.dfs.fabric.microsoft.com/{lakehouse_id}/Tables"
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
     export_schema_path = os.path.join(script_dir, "schemas", "export")
@@ -186,7 +186,7 @@ def load_northwind(env) -> None:
 
     pipeline = dlt.pipeline(
         pipeline_name="northwind",
-        destination=dlt.destinations.filesystem(bucket_url=bucket_url),
+        destination=dlt.destinations.filesystem(),#bucket_url=bucket_url),
         dataset_name=dataset_name,
         progress="enlighten",
         export_schema_path=export_schema_path,
@@ -202,9 +202,9 @@ def load_northwind(env) -> None:
 if __name__ == "__main__":
     env = sys.argv[1] if len(sys.argv) > 1 else "dev"
     
-    os.environ["CREDENTIALS__AZURE_TENANT_ID"] = os.getenv("AZURE_TENANT_ID", '"Missing')
-    os.environ["CREDENTIALS__AZURE_CLIENT_ID"] = os.getenv("AZURE_CLIENT_ID", '"Missing')
-    os.environ["CREDENTIALS__AZURE_CLIENT_SECRET"] = os.getenv("AZURE_CLIENT_SECRET", '"Missing')
+    os.environ["CREDENTIALS__AZURE_TENANT_ID"] = os.getenv("AZURE_TENANT_ID", "Missing")
+    os.environ["CREDENTIALS__AZURE_CLIENT_ID"] = os.getenv("AZURE_CLIENT_ID", "Missing")
+    os.environ["CREDENTIALS__AZURE_CLIENT_SECRET"] = os.getenv("AZURE_CLIENT_SECRET", "Missing")
     os.environ["CREDENTIALS__AZURE_STORAGE_ACCOUNT_NAME"] = "onelake"
     os.environ["CREDENTIALS__AZURE_ACCOUNT_HOST"] = "onelake.blob.fabric.microsoft.com"
 
--- sqlmesh/config.py
+++ sqlmesh/config.py
@@ -16,8 +16,8 @@ from sqlmesh.core.config import (
 from sqlmesh.core.user import User, UserRole
 from sqlmesh.integrations.github.cicd.config import GithubCICDBotConfig, MergeMethod
 
-azure_client_id = os.getenv("AZURE_CLIENT_ID", "Missing")
-azure_client_secret = os.getenv("AZURE_CLIENT_SECRET", "Missing")
+azure_client_id = os.getenv("CREDENTIALS__AZURE_CLIENT_ID", "Missing")
+azure_client_secret = os.getenv("CREDENTIALS__AZURE_CLIENT_SECRET", "Missing")
 
 fabric__warehouse_endpoint = os.getenv("FABRIC__WAREHOUSE_ENDPOINT", "Missing")
 fabric__sql_db_endpoint = os.getenv("FABRIC__SQL_DB_ENDPOINT", "Missing")
